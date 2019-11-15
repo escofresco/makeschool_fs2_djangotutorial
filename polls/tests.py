ca3df022a8f69b7ee5762d19b7469dd2f2f8f1e5
Jonasz Karwacki <jonaszakr@gmail.com>
2019-11-15 09:30:42 -0800

    add question tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
 
 import datetime
@@ -27,3 +28,40 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(seconds=23*60*60)
         recent_question = Question(pub_date=time)
         self.assertTrue(recent_question.was_published_recently())
+
+class QuestionViewTests(TestCase):
+
+    @staticmethod
+    def make_question(text, days):
+        time = timezone.now() + datetime.timedelta(days=days)
+        return Question.objects.create(question_text=text,
+                                        pub_date=time)
+
+    def test_no_question(self):
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question(self):
+        QuestionViewTests.make_question(text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_and_past_question(self):
+        QuestionViewTests.make_question(text="Past question.", days=-30)
+        QuestionViewTests.make_question(text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_two_past_questions(self):
+        QuestionViewTests.make_question(text="Past question 1.", days=-30)
+        QuestionViewTests.make_question(text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )
