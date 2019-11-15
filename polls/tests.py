75fcfa06d7a34be5071c2448747771713871405d
Jonasz Karwacki <jonaszakr@gmail.com>
2019-11-15 07:33:34 -0800

    add more comprehensive tests

--- polls/tests.py
+++ polls/tests.py
@@ -13,3 +13,17 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertFalse(future_question.was_published_recently())
+
+    def test_recently_published_old_question(self):
+        """was_published_recently should return False for pub_date older than
+        a day"""
+        time = timezone.now() - datetime.timedelta(days=1, minutes=1)
+        old_question = Question(pub_date=time)
+        self.assertFalse(old_question.was_published_recently())
+
+    def test_recently_published_recent_question(self):
+        """was_published_recently should return True for pub_date a day old or
+        newer"""
+        time = timezone.now() - datetime.timedelta(seconds=23*60*60)
+        recent_question = Question(pub_date=time)
+        self.assertTrue(recent_question.was_published_recently())
