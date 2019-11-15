b6a829200e458c4f89e69beb0b15b9b4cd4110a2
Jonasz Karwacki <jonaszakr@gmail.com>
2019-11-15 06:40:40 -0800

    add missing import

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
+
+from .models import Question
 
 
 def index(request):
